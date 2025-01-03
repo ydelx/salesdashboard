26dd0cea4d83816ad0e297029607527ead940626
ydel <ydel@luchentech.com>
2025-01-03 19:04:49 +0800

    mod:调整布局

--- src/components/sidebar/header.tsx
+++ src/components/sidebar/header.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "next/image";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,18 +6,17 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from '../ui/dropdown-menu'
-import { SearchInput } from '../ui/searchInput'
-import { Bell, ChevronDown, DiamondPercent, Menu, X } from 'lucide-react'
-import { useState } from 'react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { menu } from '@/data/menu'
-import ThemeUpdate from './themeUpdate'
+} from "../ui/dropdown-menu";
+import { Bell, ChevronDown, DiamondPercent, Menu, X } from "lucide-react";
+import { useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { menu } from "@/data/menu";
+import ThemeUpdate from "./themeUpdate";
 
 function Header() {
-  const pathname = usePathname().split('/')[1]
-  const [isSidebar, setIsSidebar] = useState(false)
+  const pathname = usePathname().split("/")[1];
+  const [isSidebar, setIsSidebar] = useState(false);
 
   return (
     <div>
@@ -32,39 +31,6 @@ function Header() {
                 <Menu />
               </div>
               <div className="flex items-center justify-between gap-x-2 h-full">
-                <div>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger>
-                      <div className="flex justify-center items-center">
-                        <Image
-                          src="/images/englishLogo.svg"
-                          alt="en"
-                          height={24}
-                          width={24}
-                          className="h-6 w-h-6"
-                        />
-                      </div>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                      <DropdownMenuLabel>Language</DropdownMenuLabel>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuItem>It</DropdownMenuItem>
-                      <DropdownMenuItem>Hin</DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </div>
-
-                <div className="flex items-center">
-                  <div className="bg-[#FFFAF1] rounded-[8px] h-10 w-10 relative">
-                    <div>
-                      <div className="absolute right-0 h-1 w-1 rounded-full bg-[#EB5757] m-1"></div>
-                    </div>
-                    <div className="flex justify-center items-center h-full">
-                      <Bell className="text-[#FFA412] h-6 w-6" />
-                    </div>
-                  </div>
-                </div>
-
                 <div className="flex h-full justify-center items-center">
                   <DropdownMenu>
                     <DropdownMenuTrigger>
@@ -123,15 +89,15 @@ function Header() {
                         className={`flex items-center 2xl:px-6 lg:px-3 px-1 2xl:py-4 lg:py-2 py-1
                           group ${
                             pathname === o.pathname
-                              ? 'text-activeText rounded-lg bg-primary dark:hover:bg-gray-700'
-                              : 'text-text rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700'
+                              ? "text-activeText rounded-lg bg-primary dark:hover:bg-gray-700"
+                              : "text-text rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
                           }`}
                       >
                         <o.icon
                           className={`${
                             pathname === o.pathname
-                              ? 'text-activeText'
-                              : 'text-text'
+                              ? "text-activeText"
+                              : "text-text"
                           }, mr-2 h-4 w-4`}
                         />
                         <span className="2xl:ms-3 lg:ms-2 ms-1 capitalize xl:text-lg lg:text-base text-sm leading-[27px]">
@@ -139,7 +105,7 @@ function Header() {
                         </span>
                       </Link>
                     </li>
-                  )
+                  );
                 })}
               </ul>
             </div>
@@ -148,52 +114,14 @@ function Header() {
       </div>
       {/*---------Desktop header---------------  */}
       <div className="md:block hidden">
-        <nav className="xl:ml-[345px] ml-[200px] fixed top-0 z-50 xl:w-[calc(100%-345px)] w-[calc(100%-200px)] bg-card border-border">
-          <div className="grid 2xl:grid-cols-3 grid-cols-2 2xl:py-[30px] py-[15px] 2xl:px-10 px-4 2xl:gap-x-2 gap-x-1 w-full">
+        <nav className="xl:ml-[250px] ml-[200px] fixed top-0 z-50 xl:w-[calc(100%-250px)] w-[calc(100%-200px)] bg-card border-border">
+          <div className="grid  grid-cols-2 2xl:py-[30px] py-[15px] 2xl:px-10 px-4 2xl:gap-x-2 gap-x-1 w-full">
             <div className="text-4xl leading-[50.4px] font-semibold flex items-center text-text capitalize">
               {pathname}
             </div>
-            <div className="2xl:block hidden">
-              <div className="flex items-center">
-                <SearchInput placeholder="Search here..." />
-              </div>
-            </div>
             <div className="flex items-center justify-end 2xl:gap-x-[14px] gap-x-2">
               <div className="flex items-center justify-between 2xl:gap-x-[42px] gap-x-2">
-                <div>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger>
-                      <div className="flex 2xl:gap-x-[14px] gap-x-2">
-                        <Image
-                          src="/images/englishLogo.svg"
-                          alt="en"
-                          height={24}
-                          width={24}
-                          className="h-6 w-h-6"
-                        />
-                        <span className="text-lg leading-[27px] text-text">
-                          Eng(US)
-                        </span>
-                        <ChevronDown className="text-text" />
-                      </div>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                      <DropdownMenuLabel>Language</DropdownMenuLabel>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuItem>It</DropdownMenuItem>
-                      <DropdownMenuItem>Hin</DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </div>
                 <div className="flex 2xl:gap-x-[14px] gap-x-2 pl-[10px] items-center justify-center">
-                  <div className="bg-[#FFFAF1] rounded-[8px] h-12 w-12 relative">
-                    <div>
-                      <div className="absolute right-0 h-2 w-2 rounded-full bg-[#EB5757] m-1"></div>
-                    </div>
-                    <div className="flex justify-center items-center h-full">
-                      <Bell className="text-[#FFA412]" />
-                    </div>
-                  </div>
                   <div className="flex 2xl:gap-x-[14px] gap-x-2">
                     <Image
                       src="/images/user.svg"
@@ -214,9 +142,6 @@ function Header() {
                           <DropdownMenuContent>
                             <DropdownMenuLabel>Profile</DropdownMenuLabel>
                             <DropdownMenuSeparator />
-                            <DropdownMenuItem>Setting</DropdownMenuItem>
-                            <DropdownMenuItem>Status</DropdownMenuItem>
-                            <DropdownMenuItem>Update Password</DropdownMenuItem>
                             <DropdownMenuItem className="flex justify-center items-center gap-x-2">
                               <ThemeUpdate />
                             </DropdownMenuItem>
@@ -234,15 +159,10 @@ function Header() {
               </div>
             </div>
           </div>
-          <div className="2xl:hidden block">
-            <div className="flex items-center p-[10px] ">
-              <SearchInput placeholder="Search here..." />
-            </div>
-          </div>
         </nav>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,15 +1,15 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { Button } from '../ui/button'
-import Header from './header'
-import { menu } from '@/data/menu'
-import { DiamondPercent } from 'lucide-react'
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
+import Header from "./header";
+import { menu } from "@/data/menu";
+import { DiamondPercent } from "lucide-react";
 
 function SidebarComponent(props: any) {
-  const pathname = usePathname().split('/')[1]
+  const pathname = usePathname().split("/")[1];
   return (
     <div>
       {/* Desktop View */}
@@ -18,7 +18,7 @@ function SidebarComponent(props: any) {
           {/* Sidebar Section */}
           <aside
             id="logo-sidebar"
-            className="fixed top-0 left-0 bottom-0 z-40 xl:w-[345px] w-[200px] h-full overflow-y-auto flex flex-col justify-between bg-card"
+            className="fixed top-0 left-0 bottom-0 z-40 xl:w-[250px] w-[200px] h-full overflow-y-auto flex flex-col justify-between bg-card"
             aria-label="Sidebar"
           >
             <div className="flex justify-center items-center w-full py-[40px]">
@@ -31,7 +31,7 @@ function SidebarComponent(props: any) {
                 </span>
               </Link>
             </div>
-            <div className="h-full 2xl:px-[46px] px-[23px] lg:pb-4 pb-2 bg-card flex flex-col justify-between">
+            <div className="h-full  px-[23px] lg:pb-4 pb-2 bg-card flex flex-col justify-between">
               <ul className="space-y-2 font-medium">
                 {menu?.map((o) => {
                   return (
@@ -39,47 +39,27 @@ function SidebarComponent(props: any) {
                       <Link
                         href={o.url}
                         className={`flex items-center 2xl:px-6 lg:px-3 px-1 2xl:py-4 lg:py-2 py-1
-                          group ${pathname === o.pathname
-                            ? 'text-activeText rounded-lg bg-primary dark:hover:bg-gray-700'
-                            : 'text-text rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700'
+                          group ${
+                            pathname === o.pathname
+                              ? "text-activeText rounded-lg bg-primary dark:hover:bg-gray-700"
+                              : "text-text rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
                           }`}
                       >
                         <o.icon
-                          className={`${pathname === o.pathname
-                              ? 'text-activeText'
-                              : 'text-text'
-                            }, mr-2 h-4 w-4`}
+                          className={`${
+                            pathname === o.pathname
+                              ? "text-activeText"
+                              : "text-text"
+                          }, mr-2 h-4 w-4`}
                         />
                         <span className="2xl:ms-3 lg:ms-2 ms-1 capitalize xl:text-lg lg:text-base text-sm leading-[27px]">
                           {o.title}
                         </span>
                       </Link>
                     </li>
-                  )
+                  );
                 })}
               </ul>
-            </div>
-            <div className="flex justify-center items-center bg-card">
-              <div
-                className="bg-primary bg-cover bg-center 2xl:w-[238px] w-[180px] h-[261px] my-10 flex flex-col justify-center items-center px-[30px] py-[46px] text-center rounded-[20px] text-white"
-                // style={{ backgroundImage: "url('images/proBackground.svg')" }}
-              >
-                <div className="bg-primary flex justify-center items-center p-[15.75px] rounded-[8px] shadow-2xl border border-activeText">
-                  <DiamondPercent className="text-activeText h-6 w-6" />
-                </div>
-                <div className="mt-2 text-xl leading-[32px] font-semibold text-activeText">
-                  DashFlow Pro
-                </div>
-                <div className="mt2 text-xs leading-4 font-medium  text-activeText">
-                  Get access to all features on tetumbas
-                </div>
-                <Button
-                  className="text-text mt-8 text-base font-semibold leading-6"
-                  variant={'secondary'}
-                >
-                  Get Pro
-                </Button>
-              </div>
             </div>
           </aside>
 
@@ -88,7 +68,7 @@ function SidebarComponent(props: any) {
         </div>
 
         {/* Main Section */}
-        <div className="p-8 xl:ml-[345px] ml-[200px] 2xl:mt-[120px] mt-[150px] bg-background min-h-[calc(100vh-130px)] h-auto w-auto">
+        <div className="p-8 xl:ml-[250px] ml-[200px] 2xl:mt-[120px] mt-[60px] bg-background min-h-[calc(100vh-130px)] h-auto w-auto">
           {props.pages}
         </div>
       </div>
@@ -99,7 +79,7 @@ function SidebarComponent(props: any) {
         {props.pages}
       </div>
     </div>
-  )
+  );
 }
 
-export default SidebarComponent
+export default SidebarComponent;
